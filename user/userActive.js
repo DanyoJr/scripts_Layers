92a31de4568f59d570c71425b4d03d2b9f85794b
Daniel Yoneshige Junior <danielyoneshigejunior@Daniels-MacBook-Air.local>
2025-02-21 12:20:01 -0300

    precisa terminar o userActive

--- user/userActive.js
+++ user/userActive.js
@@ -25,18 +25,18 @@ axios.get(`https://api.layers.digital/v1/users`, {
         
     })
 
-// axios.post(`https://api.layers.digital/v1/users//activate`,{
+axios.post(`https://api.layers.digital/v1/users/674e163365c461486e557b04/activate`,{
 
-// },{
-//             headers:{
+},{
+            headers:{
         
-//                 'Content-Type' : 'application/json',
-//                 'community-id' : `demo-escola`,    
-//                 'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJraW5kIjoiYXV0aDphY2NvdW50IiwianRpIjoiNjc4ZmU5NzMwMTg2ZjI0NDVmOWE2ZmFmIiwic3ViIjoiNjRjYTY2NzIzMGJkMzkwMDE3NzgyYmVkIiwiaXNzIjoiaHR0cHM6Ly9pZC5sYXllcnMuZGlnaXRhbCIsImlhdCI6MTczNzQ4NDY1OTI3MH0.FuqZ7L5hMpvoO4V2CgPyN3X9pCEAA5RE6pP8Gy8IUgk' ,
-//                 //'secure' : true,
+                'Content-Type' : 'application/json',
+                'community-id' : `junao-school`,    
+                'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJraW5kIjoiYXV0aDphY2NvdW50IiwianRpIjoiNjc4ZmU5NzMwMTg2ZjI0NDVmOWE2ZmFmIiwic3ViIjoiNjRjYTY2NzIzMGJkMzkwMDE3NzgyYmVkIiwiaXNzIjoiaHR0cHM6Ly9pZC5sYXllcnMuZGlnaXRhbCIsImlhdCI6MTczNzQ4NDY1OTI3MH0.FuqZ7L5hMpvoO4V2CgPyN3X9pCEAA5RE6pP8Gy8IUgk' ,
+                //'secure' : true,
         
-//             }
-//         })
+            }
+        })
 
 
 }catch(err){
